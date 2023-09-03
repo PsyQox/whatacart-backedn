ddef0c38d503dc2d36d2f32f4f513a26ce2180cb
PsyQox <pikithor@gmail.com>
2023-09-02 19:38:10 -0500

    DP

--- src/db.js
+++ src/db.js
@@ -6,7 +6,9 @@ const RolModel = require('./models/Rol')
 const UserModel = require('./models/User')
 const SuperuserModel = require('./models/Superuser')
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/db_whatacart`,{logging:false, native: false})
+// const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/db_whatacart`,{logging:false, native: false})
+// postgres://fl0user:FN3gLZ9Prvmq@ep-green-pine-55017894.us-east-2.aws.neon.tech:5432/whatacart-db?sslmode=require
+const sequelize = new Sequelize(`postgres://fl0user:FN3gLZ9Prvmq@ep-green-pine-55017894.us-east-2.aws.neon.tech:5432/whatacart-db?sslmode=require`)
 
 BusinessModel(sequelize)
 RolModel(sequelize)
