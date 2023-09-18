0efd0c5bb2622cfb95c3d05d7e18ae0cfac39e78
PsyQox <pikithor@gmail.com>
2023-09-17 23:32:09 -0500

    aja

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
 const server = require('./src/app.js');
 const { conn } =require('./src/db.js')
+const {PORT} = process.env
 
-  server.listen(3001, async () => {
+  server.listen(PORT, async () => {
     await conn.sync({force:false}) 
-    console.log('%s listening at 3001')
+    console.log(`%s listening at ${PORT}`)
   })
--- src/db.js
+++ src/db.js
@@ -2,7 +2,6 @@ require('dotenv').config()
 const { Sequelize } = require('sequelize')
 const {DB_USER, DB_PASSWORD, DB_HOST,DATABASE_URL} = process.env
 const BusinessModel = require('./models/Business')
-const RolModel = require('./models/Rol')
 const UserModel = require('./models/User')
 const SuperuserModel = require('./models/Superuser')
 
@@ -11,7 +10,6 @@ const SuperuserModel = require('./models/Superuser')
 const sequelize = new Sequelize(DATABASE_URL,{dialect:"postgres",logging:false})
 
 BusinessModel(sequelize)
-RolModel(sequelize)
 UserModel(sequelize)
 SuperuserModel(sequelize)
 
@@ -28,17 +26,11 @@ User.belongsTo(Business)
 Business.hasMany(User)
 
 
-Rol.belongsTo(Business)
-Business.hasMany(Rol)
 
-//Third: User belongs to many Rol, and Rol belongs to many User, N:M, Mid table
-User.belongsToMany(Rol, { through: 'user_rol', timestamps: false})
-Rol.belongsToMany(User, { through: 'user_rol', timestamps: false})
 
 module.exports={
     User,
     Business,
-    Rol,
     Superuser,
     conn: sequelize,
 }
